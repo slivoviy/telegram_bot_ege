582e1447193c49f12b45468217f527d15e279ff1
slivoviy <55319230+slivoviy@users.noreply.github.com>
2020-02-08 17:18:07 +0500

    commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python
+sample.py
--- sample.py
+++ sample.py
@@ -2,21 +2,21 @@ from telegram.ext import Updater         # пакет называется pytho
 from telegram.ext import CommandHandler  # модуль почему-то просто telegram ¯\_(ツ)_/¯
 
 TOKEN = '1027639107:AAGMVghxLvsqHF-36i_y_hk-Nvc0Qn-zak0'
-REQUEST_KWARGS={
-    'proxy_url': 'socks5://s5.priv.opennetwork.cc:1080',
-    # Optional, if you need authentication:
-    'urllib3_proxy_kwargs': {
-        'username': 'v3_470788620',
-        'password': 'BJBANUXu',
-    }
-}
+# REQUEST_KWARGS={
+#     'proxy_url': 'socks5://s5.priv.opennetwork.cc:1080',
+#     # Optional, if you need authentication:
+#     'urllib3_proxy_kwargs': {
+#         'username': 'v3_470788620',
+#         'password': 'BJBANUXu',
+#     }
+# }
 
 def start(bot, update):
     # подробнее об объекте update: https://core.telegram.org/bots/api#update
     bot.sendMessage(chat_id=update.message.chat_id, text="Здравствуйте.")
 
 
-updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS)  # тут токен, который выдал вам Ботский Отец!
+updater = Updater(TOKEN)  # тут токен, который выдал вам Ботский Отец!
 
 start_handler = CommandHandler('start', start)  # этот обработчик реагирует
                                                 # только на команду /start
